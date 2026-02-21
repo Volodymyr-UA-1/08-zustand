60f7cfde1c8c0d93d5f15166d6c120351e466b5e
Volodymyr Salashchenko <salashchenkovladymyr@gmail.com>
2026-02-21 18:34:19 +0200

    add Metedata, Roboto, OG

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,37 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import Header from "@/components/Header/Header"; // Повертаємо імпорт
 import Footer from "@/components/Footer/Footer"; // Повертаємо імпорт
 import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
 import "./globals.css";
+import { Roboto } from 'next/font/google';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://notehub.com"),
   title: "NoteHub",
   description: "Manage your notes efficiently",
+  openGraph: {
+    title:"NoteHub",
+      description:"Manage your notes efficiently",
+      url:`https://notehub.com`,
+      images: [
+        {
+      url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+          height: 630,
+          alt: "NoteHub",
+      
+    },
+    ],
+    type: 'website',
+  }
 };
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-roboto',
+  display: 'swap',
+});
 
 export default function RootLayout({
   children,
@@ -29,7 +42,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={roboto.variable}>
         <TanStackProvider>
           {/* Header має бути тут, щоб бути видимим на всіх сторінках */}
           <Header /> 
